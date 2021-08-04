3f7b376453b64521cb48297c584f802d747109d4
kenna-hwa <76397046+kenna-hwa@users.noreply.github.com>
2021-08-05 00:40:53 +0900

    드롱기 모달창 desktop 버전에 제작

--- mobilefirst/js/index.js
+++ mobilefirst/js/index.js
@@ -207,3 +207,25 @@ mobileVip.addEventListener("mouseout", function(){
 mobileVipBox.addEventListener("mouseout", function(){
     mobileVipBox.style.display = "none";
 })
+
+
+//modal
+const body = document.querySelector('body'); 
+const modal = document.querySelector('.modal_wrap'); 
+const closeBtn = document.querySelector('.modal_close_btn');
+
+window.addEventListener('load', () => {
+    modal.classList.add('active');
+
+    if (modal.classList.contains('active')) {
+        body.style.overflow = 'hidden';
+    }
+  });
+
+  closeBtn.addEventListener('click', () => {
+      modal.classList.remove('active');
+
+      if (!modal.classList.contains('active')) {
+        body.style.overflow = 'auto';
+    }
+  });
